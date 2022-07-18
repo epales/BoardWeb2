bbe83dabf8a75dfcdbd7ebdd8c40eb99dbd11e41
Boae <aoekaze@naver.com>
2022-07-18 15:00:33 +0900

    장바구니 구현 완료

---
+++ src/test/java/com/ezen/JoinTest.java
@@ -0,0 +1,17 @@
+package com.ezen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinTest {
+
+	@Test
+	public void joinTest() {
+		System.out.println("featuer/2 브랜치를 위한 테스트");
+		System.out.println("issue#2 장바구니 구현 완료");
+	}
+}
